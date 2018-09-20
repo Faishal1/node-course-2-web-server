af78073af81b9460153e430be1374e8ad72d7558
Faishal <tahseen@blickx.com>
2018-09-20 17:41:22 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  var log = `${now} ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + '\n', err => {
+    if (err) {
+      console.log("Unable to append ");
+    }
+  });
+  next();
+});
+
+app.get("/", (req, res) => {
+  // res.send("<h1>Hello Express!</h1>");
+  // res.send({
+  //     name: "andrew",
+  //     likes: [
+  //         'biking',
+  //         'cities'
+  //     ]
+  // })
+  res.render("home.hbs", {
+    pageTitle: "HOMEPAGE HAI YEH SMAJHA KYA",
+    message: "Welcome to the HOMEPAGE"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    pageTitle: "About Page re baba"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App is listening to port 3000");
+});
